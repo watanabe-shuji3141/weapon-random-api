3e17207dd450afaf74248f71992065213e0075ef
watanabe-shuji3141 <167500555+watanabe-shuji3141@users.noreply.github.com>
2026-03-17 07:37:30 +0900

    Update random.js
    resultの出力方法修正

--- api/random.js
+++ api/random.js
@@ -29,7 +29,8 @@ export default async function handler(req, res) {
 
   const weapon = randomWeapon()
 
-  result += `${name}（${weapon.name}）\n`
+  // result += `${name}（${weapon.name}）\n`
+  result += `${name}（${weapon.name.ja_JP}）\n`
 
  }
 

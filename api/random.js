d038390f280a1fb104799de78735ddbf6f4d736a
watanabe-shuji3141 <167500555+watanabe-shuji3141@users.noreply.github.com>
2026-03-23 21:07:39 +0900

    Update redirect location with query parameters

--- api/random.js
+++ api/random.js
@@ -1,9 +1,15 @@
 // /api/random.js
 export default function handler(req, res) {
-  const unique = Date.now();
+  const { count, names } = req.query;
+
+  const query = new URLSearchParams({
+    count,
+    names,
+    t: Date.now()
+  }).toString();
 
   res.writeHead(302, {
-    Location: `/api/main?t=${unique}`
+    Location: `/api/main?${query}`
   });
   res.end();
 }
